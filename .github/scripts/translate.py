35ca9f5383deab5682c295a90a603b570ace494d
Thilo Graf <dbt@novatux.de>
2024-05-12 18:36:05 +0200

    .translate.py: update link to repo

--- .github/scripts/translate.py
+++ .github/scripts/translate.py
@@ -6,7 +6,7 @@ def translate_readme(input_text, target_lang):
     translated_text = translated.text
     
     # add hint for automatically translation
-    translated_text = f"Note: This is an automatically translated file. Original content from [here](https://github.com/dbt1/docker-tuxbox-build/blob/master/README-de.md):\n\n{translated_text}"
+    translated_text = f"Note: This is an automatically translated file. Original content from [here](https://github.com/tuxbox-neutrino/docker-buildenv/blob/master/README-de.md):\n\n{translated_text}"
 
     # Use this workaround, because translater breaks some links and anchors
     translated_text = translated_text.replace("[Build Image](#Build Image)", "[Build Image](#build-image)")
